dd29c877d710fd52f01c0a20cebc4c2e77537b49
Harini-67 <162011826+Harini-67@users.noreply.github.com>
2024-11-02 11:50:17 +0530

    Add files via upload
    import java.util.Scanner;
    
    public class SimpleCalculator {
    
        public static void main(String[] args) {
            Scanner scanner = new Scanner(System.in);
    
            // Prompt the user for two numbers
            System.out.print("Enter the first number: ");
            double num1 = scanner.nextDouble();
    
            System.out.print("Enter the second number: ");
            double num2 = scanner.nextDouble();
    
            // Prompt the user for an operation
            System.out.print("Choose an operation (+, -, *, /): ");
            char operation = scanner.next().charAt(0);
    
            double result;
    
            // Perform the selected operation
            switch (operation) {
                case '+':
                    result = num1 + num2;
                    break;
                case '-':
                    result = num1 - num2;
                    break;
                case '*':
                    result = num1 * num2;
                    break;
                case '/':
                    if (num2 != 0) {
                        result = num1 / num2;
                    } else {
                        System.out.println("Error! Division by zero is not allowed.");
                        return;
                    }
                    break;
                default:
                    System.out.println("Invalid operation!");
                    return;
            }
    
            // Display the result
            System.out.println("The result is: " + result);
        }
    }

---
+++ newfile.java
@@ -0,0 +1,13 @@
+class main{
+	public static void main(String args[]){
+	    String RCB="win";
+	    if(RCB=="win")
+	    {
+	        System.out.print("Happy");
+	    }
+	    else
+	    {
+	        System.out.print("lose");
+	    }
+	}
+}
